1a4ce9af8e004ce0b83e350b391ff3a93c950f80
Dwade <jchedewei@163.com>
2019-01-20 15:20:01 +0800

    add project files

---
+++ applications/main.c
@@ -0,0 +1,26 @@
+/*
+ * File      : main.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2009, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2015-07-29     Arda.Fu      first implementation
+ */
+#include <rtthread.h>
+#include <board.h>
+
+int main(void)
+{
+    /* user app entry */
+		int a=0;
+		int b=1;
+		int c;
+		c = a + b;
+		a = c;
+    return 0;
+}
